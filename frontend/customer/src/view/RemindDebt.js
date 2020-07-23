5a86d5dd2dc17b783064eca09d3e5a5775037ff5
Lam Duy Lac <laclam@chotot.vn>
2020-07-23 15:41:24 +0700

    Add switch ui

--- frontend/customer/src/view/RemindDebt.js
+++ frontend/customer/src/view/RemindDebt.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Row, Accordion, Col, Container } from "react-bootstrap";
+import { Card, ListGroup, Row, Accordion, Col, Container } from "react-bootstrap";
 import DetailDebtor from './DetailDebtor.js';
 import InternalReceiver from './InternalReceiver.js';
 import {pickDebtor} from '../action/Debtor.js'
@@ -8,8 +8,25 @@ import store from '../store/init.js'
 import {setReceivers} from '../action/Receiver.js'
 import axios from 'axios';
 
+class SwitchUI extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    if (this.props.typeSelect && this.props.typeSelect == "1") {
+      return <DetailDebtor/>
+    }
+    return <div>1</div>
+  }
+}
 
 class RemindDebt extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       typeSelect:"1",
+     }
+  }
   callAPI() {
     axios({
       method:`get`,
@@ -35,7 +52,7 @@ class RemindDebt extends React.Component {
             <Col xs={6} md={4}>
               <Accordion defaultActiveKey="0">
                 <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="0">
+                  <Accordion.Toggle as={Card.Header} eventKey="0" onClick={() => {this.setState({typeSelect:"1"})}}>
                     Danh sách nội bộ
                   </Accordion.Toggle>
                   <Accordion.Collapse eventKey="0">
@@ -44,9 +61,28 @@ class RemindDebt extends React.Component {
                     </Card.Body>
                   </Accordion.Collapse>
                 </Card>
+                <Card>
+                  <Accordion.Toggle as={Card.Header} eventKey="1" onClick={() => {this.setState({typeSelect:"2"})}}>
+                    Khác
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                      <ListGroup>
+                        <ListGroup.Item action onClick={() => {this.setState({typeSelect:"2"})}}>
+                          Danh sách nợ
+                        </ListGroup.Item>
+                        <ListGroup.Item action onClick={() => {this.setState({typeSelect:"3"})}}>
+                          Danh sách tôi nợ
+                        </ListGroup.Item>
+                      </ListGroup>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
               </Accordion>
             </Col>
-            <Col xs={12} md={8}><DetailDebtor/></Col>
+            <Col xs={12} md={8}>
+              <SwitchUI typeSelect={this.state.typeSelect}/>
+            </Col>
           </Row>
         </Container>
       </div>
