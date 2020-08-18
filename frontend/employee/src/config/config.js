640a199ef5b68464aba673958c8a26e51bb063b0
Lam Duy Lac <lamduylac@gmail.com>
2020-08-18 08:02:51 +0700

    Add package

--- frontend/employee/src/config/config.js
+++ frontend/employee/src/config/config.js
@@ -1,5 +1,5 @@
 const Config = {
-  BEUrl: 'http://localhost:9000',
+  BEUrl: 'https://ibanking37.herokuapp.com',
   LocalBEUrl: 'http://localhost:9000',
   CloudflareBEUrl: 'http://ibanking.imjet.vn',
 } 
